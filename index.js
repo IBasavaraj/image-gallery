13b69511daf9be25fa58f258c25e541f45f1bcb6
IBasavaraj <105215381+IBasavaraj@users.noreply.github.com>
2022-05-10 09:37:54 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let mainImg = document.getElementById("main__img");
+let images = document.querySelectorAll(".image");
+
+images.forEach(imgs =>{       
+    imgs.addEventListener("click", (e) => {
+        images.forEach(image => image.style.border = "none");      
+        mainImg.src = e.target.src; 
+        e.target.style.border = "3px solid red";                                    
+    });        
+});
+
+
+
+
+
